0730d42d0bafe40e508816907105a6f8a30e4096
giacomohrd <giacomo.accardi.lavoro@gmail.com>
2024-07-09 15:50:29 +0200

    [UPD] There's now some instruction into js file

---
+++ script.js
@@ -0,0 +1,15 @@
+/* 
+---------------------------------------Instructions--------------------------------------------------------------
+
+1. Creare un'array vuoto che conterrà cinque numeri generati casualmente
+    >> Creare una funzioni che generi questi cinque numeri casuali e che li inserisca all'interno dell'array 
+2. Mostrare i numeri sulla pagina
+   >> Attraverso l'uso di un timer, i numeri, spariranno dopo 30 secondi.
+3. Chiedere all'utente di inserire i numeri precedentemente scomparsi tramite prompt
+    >> Raccogliere i dati inseriti dall'utente in un nuovo array 
+       (l'obbiettivo è quello di scrivere esattamente gli stessi numeri)
+4. Confrontare i numeri apparsi all'inizio con quelli inseriti dall'utente
+5. Restituire in pagina quanti e quali numeri sono stati indovinati
+
+---------------------------------------------------------------------------------------------------------------
+*/
